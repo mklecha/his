e30a3ecd411498ffb69f3b9728535ebb345fe2db
michalklecha <michalklecha.0@gmail.com>
2018-01-18 09:46:40 +0100

    admin tabs added

--- src/main/java/pl/michalklecha/his/controllers/AdminController.java
+++ src/main/java/pl/michalklecha/his/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package pl.michalklecha.his.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,15 @@ public class AdminController {
     @RequestMapping(path = "/admin.html")
     public String admin() {
         return "admin/admin";
+    }
+
+    @RequestMapping(path = "/manage-gifts.html")
+    public String gifts(Model model) {
+        return "admin/gifts";
+    }
+
+    @RequestMapping(path = "/manage-invitations.html")
+    public String invitations(Model model) {
+        return "admin/invitations";
     }
 }
---
+++ src/main/resources/templates/admin/gifts.ftl
@@ -0,0 +1,18 @@
+<table class="table">
+    <tr>
+        <th width="5%">Id</th>
+        <th width="15%">Nazwa</th>
+        <th width="10%">Link</th>
+        <th width="45%">Opis</th>
+        <th width="10%"></th>
+        <th width="15%"></th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>3</td>
+        <td>3</td>
+        <td>3</td>
+    </tr>
+</table>
