0913ed7325ffc6a92854d84ccd4e586d64908a56
michalklecha <michalklecha.0@gmail.com>
2018-01-18 01:36:11 +0100

    js refactor

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
     /* Wow */
     new WOW().init();
+    checkScroll();
+});
+
+$(window).scroll(function() {
+    checkScroll();
 });
 
 // jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
+function checkScroll() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+}
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
