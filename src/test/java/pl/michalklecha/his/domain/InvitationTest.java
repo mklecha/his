5afa3dd93a9352c30e4977f2ae56a3cdad65f32b
michalklecha <michalklecha.0@gmail.com>
2018-02-09 02:01:39 +0100

    h2 tests

---
+++ src/test/java/pl/michalklecha/his/domain/InvitationTest.java
@@ -0,0 +1,39 @@
+package pl.michalklecha.his.domain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.michalklecha.his.domain.model.Invitation;
+import pl.michalklecha.his.domain.repositories.InvitationRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class InvitationTest {
+
+    @Autowired
+    private InvitationRepository invitationRepository;
+
+    @Test
+    public void isEmpty() {
+        List<Invitation> invitations = invitationRepository.findAll();
+        assertThat(invitations).isEmpty();
+    }
+
+    @Test
+    public void saveAndFetchInvitation() {
+        Invitation invitation = new Invitation("key", "message", false);
+
+        invitationRepository.save(invitation);
+
+        Invitation fetchedInvitation = invitationRepository.findOne(invitation.getPageKey());
+
+        assertThat(fetchedInvitation).isNotNull();
+    }
+}
